9948b9b38207708bfc5bc5ffcd1917ac05d39b18
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-11-07 23:52:07 +0600

    replace triangle sections js with js-function

--- js/geometry.js
+++ js/geometry.js
@@ -10,34 +10,23 @@ function setElementInnerText(elementId, area){
     element.innerText = area;
 }
 
-
+// Triangle
 function calculateTriangleArea(){
-    const baseField = document.getElementById('triangle-base');
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
-    // console.log(base);
-
-    const heightField = document.getElementById('triangle-height');
-    const heightValueFeild = heightField.value;
-    const height = parseFloat(heightValueFeild);
-    // console.log(height);
-
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height');
     const area = 0.5 * base * height;
-    // console.log(area);
-
-    const areaSpan = document.getElementById('triangle-area');
-    areaSpan.innerText = area;
-    // console.log(areaSpan.innerText);
+    setElementInnerText('triangle-area', area);
 }
+
+// Rectangle
 function calculateRectangleArea(){
     const width = getInputValue('rectangle-width');
-
     const length = getInputValue('rectangle-length');
-
     const area = width * length;
     setElementInnerText('rectangle-area',area);
 }
 
+// Parallelogram
 function calculateParallelogramArea(){
     const base = getInputValue("parallelogram-base");
     const height = getInputValue("parallelogram-height");
@@ -46,6 +35,7 @@ function calculateParallelogramArea(){
     
 }
 
+// Ellipse
 function calculateEllipseArea(){
     const majorRadious = getInputValue('ellipse-major-radious');
     const minorRadious = getInputValue('ellipse-minor-radious');
@@ -54,7 +44,7 @@ function calculateEllipseArea(){
     setElementInnerText("ellipse-area", areaToDecimal);
 }
 
-
+// Rhombus
 function calculateRhombusArea(){
     const majorHand = getInputValue('rhombus-major-hand');
     const minorHand = getInputValue('rhombus-minor-hand');
@@ -63,8 +53,8 @@ function calculateRhombusArea(){
     setElementInnerText("rhombus-area", areaToDecimal);
 }
 
-
-function calculatePentagonArea(){
+// Rhombus
+function calculateRhombusArea(){
     const majorHand = getInputValue('pentagon-major-hand');
     const minorHand = getInputValue('pentagon-minor-hand');
     const area = 0.5 * majorHand * minorHand;
